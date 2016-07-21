e3e45ba9845a51da5dc887d8dac2d4d5e12a9314
deskach <dzianis.kachanau@gmail.com>
2016-07-22 08:09:42 +1000

    introduced filtering buttons

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router';
 
 export default class App extends Component {
   render() {
     return (
       <div>
+        <Link to="/comics" className="btn btn-primary filtering-button">Comics</Link>
+        <Link to="/characters" className="btn btn-primary filtering-button">Characters</Link>
         {this.props.children}
       </div>
     );
