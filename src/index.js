c81190bef135ae854b54098221cd19886d4ae4fd
deskach <dzianis.kachanau@gmail.com>
2016-07-22 05:47:56 +1000

    introduced Routing

--- src/index.js
+++ src/index.js
@@ -2,16 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { Router, browserHistory } from 'react-router';
 import ReduxPromise from 'redux-promise';
 
-import Comics from './components/comics';
 import reducers from './reducers';
+import routes from './routes';
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 //^if the payload is a promise redux_promise will wait for the response and put it in place (see doFetchContent action)
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <Comics />
+    <Router history={browserHistory} routes={routes} />
   </Provider>
   , document.querySelector('.container'));
