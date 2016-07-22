0d3ac4d37481f05e6c163851b991b696ac67cbc4
deskach <dzianis.kachanau@gmail.com>
2016-07-22 12:33:17 +1000

    removed search bar from the list of characters

--- src/components/characters.js
+++ src/components/characters.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-import SearchBar from '../containers/search_bar';
+// import SearchBar from '../containers/search_bar';
 import CharacterList from '../containers/character_list';
-import actionConstants from '../actions/constants';
+// import actionConstants from '../actions/constants';
 
 export default class Characters extends React.Component {
   render() {
     return (
       <div>
-        <SearchBar contentType={ actionConstants.CONTENT_TYPE.CHARACTERS } />
+        {/*<SearchBar contentType={ actionConstants.CONTENT_TYPE.CHARACTERS } />*/}
         <CharacterList />
       </div>
     );
