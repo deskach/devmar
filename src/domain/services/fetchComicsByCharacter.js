21f576bf4a7bb488031f6eae9842dc840ebf52e5
deskach <dzianis.kachanau@gmail.com>
2016-07-24 23:13:07 +1000

    removed debugging code

--- src/domain/services/fetchComicsByCharacter.js
+++ src/domain/services/fetchComicsByCharacter.js
@@ -17,16 +17,12 @@ export function fetchComicsByCharacter(options = {}) {
   var searchParams = {nameStartsWith: options.term};
   var url = getSignedUrl4Collection(charactersSubUrl, searchParams);
 
-  console.log(url);
-
   var request = axios.get(url).then((response) => {
     if (response.data.data.results) {
       const id = response.data.data.results[0].id;
       const collection = `${charactersSubUrl}/${id}/${comicsSubUri}`;
 
-      console.log(collection);
       var url1 = getSignedUrl4Collection(collection);
-      console.log(url1);
 
       return axios.get(url1);
     }
